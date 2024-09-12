4efadbfa2e65a213966a030346cf1e81b88ba47e
AdamSeungheonShin <ssh9809@naver.com>
2024-09-12 17:58:50 +0900

    chore: tailwind setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,8 @@
 export default function Home() {
   return (
-    <div className="">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        hi
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        bye
-      </footer>
+    <div className="w-500 h-500 mx-auto bg-yzy-600">
+      <main>hi</main>
+      <footer>bye</footer>
     </div>
   )
 }
