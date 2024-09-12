71691fdb0901d77e610a681df01b672e951cf860
AdamSeungheonShin <ssh9809@naver.com>
2024-09-12 17:33:42 +0900

    chore: fix lint error.. idk how I made it

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next"
 import localFont from "next/font/local"
+import React from "react"
 
 import "./globals.css"
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
 export default function Home() {
   return (
     <div className="">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start"></main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center"></footer>
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        hi
+      </main>
+      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+        bye
+      </footer>
     </div>
   )
 }
